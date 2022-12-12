7b82fc95c3380191aef55e974763cac8ad17a9ec
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-12-11 19:46:30 -0600

    Migrate from english to spanish

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,70 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { BiTask } from 'react-icons/bi';
-import { TfiAgenda } from 'react-icons/tfi';
-import { AiOutlineSchedule } from 'react-icons/ai';
-import { MdOutlineSchedule } from 'react-icons/md';
+import { Fragment } from 'react';
 
-import styles from 'styles/home.module.scss';
-import { Layout } from 'components/layout/Layout';
+import styles from 'styles/pages/home.module.scss';
+import { useFeatures } from 'hooks/useFeatures';
 
 const {
   homeContent,
   titleText,
-  welcomeText,
-  features,
+  text,
+  featuresLayout,
   articleFeature,
   featureTitle,
+  subtitleText,
 } = styles;
 
 export default function Home() {
+  const { features, logo } = useFeatures();
+
   return (
     <>
       <Head>
-        <title> Scheduler - Tool for day to day organization </title>
+        <title> Planner - Herramienta estudiantil para la organización </title>
         <meta
           name="description"
-          content="Scheduler, free tool where you can create, organize, edit and view your day to day with different utilities, like tasks maker, scheduler and agenda"
+          content="Planner, herramienta gratuita para el estudiante que busca mejorar su organización en el día a día"
         />
       </Head>
       <article className={homeContent}>
         <h1 className={titleText}>
-          <MdOutlineSchedule />
-          scheduler
+          {logo.icon}
+          {logo.title}
         </h1>
-        <p className={welcomeText}>
-          A great tool for day to day organization. <br />
-          Quickly create, edit and view your schedule.
+        <p className={text}>
+          {logo.speech.map((speech, i) => (
+            <Fragment key={'homegreeting:' + i}>
+              {speech}
+              {i < logo.speech.length - 1 && <br />}
+            </Fragment>
+          ))}
         </p>
       </article>
-      <div className={features}>
-        <Link className={articleFeature} href="/schedule">
-          <h2 className={featureTitle}>
-            <AiOutlineSchedule /> Schedule
-          </h2>
-          <p>
-            Manage your college subjects, add different fields for each one,
-            including the hours in which you are taking the class, teacher name,
-            classroom location, subject name, subject grades.
-          </p>
-        </Link>
-        <Link className={articleFeature} href="/agenda">
-          <h2 className={featureTitle}>
-            <TfiAgenda /> Agenda
-          </h2>
-          <p>
-            Store your future events, keep updated of the ones that are close
-            and receive notifications according to what is better for you. You
-            can also store important dates related to your school subjects.
-          </p>
-        </Link>
-        <Link className={articleFeature} href="/tasks">
-          <h2 className={featureTitle}>
-            <BiTask /> Tasks
-          </h2>
-          <p>
-            {`Organize your pending tasks, make a list of the ones you've
-            completed and the ones you haven't, it is ideal for homework
-            organization, specially if you want to segment a homework into tiny
-            pieces.`}
-          </p>
-        </Link>
+      <div className={featuresLayout}>
+        {features.map((feature, i) => (
+          <Link
+            className={articleFeature}
+            href={feature.path}
+            key={feature.path + `feature:${i}`}
+          >
+            <h2 className={featureTitle}>
+              {feature.icon}
+              {feature.title}
+            </h2>
+            <p>{feature.description}</p>
+          </Link>
+        ))}
       </div>
+      <article className={homeContent}>
+        <h2 className={subtitleText}>Empieza hoy!</h2>
+        <p className={text}>
+          <Link href={'/register'}> Crea una cuenta </Link>{' '}
+          {`en caso de no contar con una`} <br />O
+          <Link href={'/register'}> inicia sesión </Link> si ya cuentas con una
+          cuenta existente
+        </p>
+      </article>
     </>
   );
 }
