225cf30d9d64a632d05e7350a5025991ca06297b
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-12-11 19:38:48 -0600

    Change to spanish lang

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import styles from 'styles/layout/footer.module.scss';
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <span> 2022 ~ Created by @AmauryVasquez </span>
+      <span> 2022 ~ Creado por @AmauryVasquez </span>
     </footer>
   );
 };
